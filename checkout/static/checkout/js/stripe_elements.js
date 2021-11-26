f06e5eb985f7e7917b51432867f0382581b2f907
johnroutledge <john_routledge@hotmail.com>
2021-11-26 22:46:09 +0000

    fix card element styling

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,7 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
+
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
@@ -10,15 +11,15 @@ var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var style = {
-    base: {
-        color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
-        }
-    },
+     base: {
+         color: '#000',
+         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+         fontSmoothing: 'antialiased',
+         fontSize: '16px',
+         '::placeholder': {
+             color: '#aab7c4'
+         }
+     },
     invalid: {
         color: '#dc3545',
         iconColor: '#dc3545'
--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'sk_test_51Jzr2fHmTUV1jLZyFuHYMnzw57YzM4eBIo7umDp7itPEx1dSXWSA7Fwi8Q9cnzlHXcYlCzqZnGJE7RMCx5ARqGpH003Lq0nI3G',
+        # 'stripe_public_key': 'sk_test_51Jzr2fHmTUV1jLZyFuHYMnzw57YzM4eBIo7umDp7itPEx1dSXWSA7Fwi8Q9cnzlHXcYlCzqZnGJE7RMCx5ARqGpH003Lq0nI3G',
+        'stripe_public_key': 'pk_test_51Jzr2fHmTUV1jLZyRMJFfHLtqputaOu5DoviFsb0KETFc2HvmcjTEqtpWiLRbL2aUlX5be8A37gabOUSJU1eiUMP00cMWT6Pv',
         'client_secret': 'test client secret',
     }
 
