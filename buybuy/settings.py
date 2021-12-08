e3824d576b3c61c23de7d16eee1fd003d73de893
johnroutledge <john_routledge@hotmail.com>
2021-12-08 21:10:55 +0000

    add deployment settings

--- buybuy/settings.py
+++ buybuy/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-n8doj4bf6+5p-ow&c9(s(7s!0jg#0+@jky280n0cmdc2qexg9e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jr-buybuy.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,18 @@ WSGI_APPLICATION = 'buybuy.wsgi.application'
 Database
 https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
